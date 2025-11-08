f0f23cddb26b6f9d4b560f0dac40b9185de5ff6b
smolsquirrel936 <smolsquirrel936@gmail.com>
2025-11-08 17:26:36 +0800

    Change the field type of instruction and minor coding style fixed

--- CPU.cpp
+++ CPU.cpp
@@ -3,43 +3,69 @@
 namespace RV32IM {
   CPU::CPU(Memory* p_TheMemory): TheMemory(p_TheMemory) {}
   void CPU::Fetch() {
-    MemoryAddressRegister = ProgramCounter;
-    MemoryDataRegister = TheMemory->Read(MemoryDataRegister);
-    InstructionRegister = MemoryDataRegister;
-    ProgramCounter += 1;
+    MAR = PC;
+    MDR = TheMemory->Read(MDR);
+    IR = MDR;
+    PC += 1;
   }
-  int32_t CPU::DecodeImm(uint8_t p_Opcode) {
-    switch(p_Opcode) {
-      case 0x13: return SignExtend::extend(InstructionRegister >> 20, 12); // ALU imm
-      case 0x03: return SignExtend::extend(InstructionRegister >> 20, 12); // LOAD
-      case 0x23: return SignExtend::extend(((InstructionRegister >> 25) << 5) | ((InstructionRegister >> 7) & 0x1F), 12); // STORE
-      case 0x63: return SignExtend::extend(((InstructionRegister >> 31) << 12) | (((InstructionRegister >> 7) & 0x01) << 11) | (((InstructionRegister >> 25) & 0x3F) << 5) | (((InstructionRegister >> 8) & 0x0F) << 1), 11); // BRANCH
-      case 0x37: return (InstructionRegister & ((1 << 20) - 1)); // LUI
-      case 0x17: return (InstructionRegister & ((1 << 20) - 1)); // AUIPC
-      case 0x6F: return SignExtend::extend(((InstructionRegister >> 31) << 20) | (((InstructionRegister >> 12) & 0xFF) << 12) | (((InstructionRegister >> 20) & 0x01) << 11) | (((InstructionRegister >> 25) & 0x3F) << 5) | (((InstructionRegister >> 21) & 0x0F) << 1), 12); // JAL
-      default: return 0;
+  int32_t CPU::DecodeImm(std::bitset<7> p_Opcode) {
+    uint8_t uint8_opcode = static_cast<uint8_t>(p_Opcode.to_ulong());
+    switch(uint8_opcode) {
+      case 0x13: // ALU imm
+        return SignExtend::extend (IR >> 20, 12);
+
+      case 0x03: // LOAD
+        return SignExtend::extend (IR >> 20, 12);
+
+      case 0x23: // STORE
+        return SignExtend::extend (((IR >> 25) << 5) | ((IR >> 7) & 0x1F), 12);
+
+      case 0x63: // BRANCH
+        return SignExtend::extend (((IR >> 31) << 12) | (((IR >> 7) & 0x01) << 11)
+                                        | (((IR >> 25) & 0x3F) << 5)
+                                        | (((IR >> 8) & 0x0F) << 1),
+                                    11);
+
+      case 0x37: // LUI
+        return (IR & ((1 << 20) - 1));
+
+      case 0x17: // AUIPC
+        return (IR & ((1 << 20) - 1));
+
+      case 0x6F: // JAL
+        return SignExtend::extend (((IR >> 31) << 20) | (((IR >> 12) & 0xFF) << 12)
+                                        | (((IR >> 20) & 0x01) << 11)
+                                        | (((IR >> 25) & 0x3F) << 5)
+                                        | (((IR >> 21) & 0x0F) << 1),
+                                    12);
+
+      default:
+        return 0;
     }
   }
-  Instruction CPU::Decode() {
+  Instruction CPU::Decode() { 
     Instruction instr {};
-    instr.opcode = InstructionRegister & 0x7F;
-    instr.rd = (InstructionRegister >> 7) & 0x1F;
-    instr.funct3 = (InstructionRegister >> 12) & 0x07;
-    instr.rs1 = (InstructionRegister >> 15) & 0x1F;
-    instr.rs2 = (InstructionRegister >> 20) & 0x1F;
-    instr.funct7 = (InstructionRegister >> 25) & 0x7F;
+    instr.opcode = IR & 0x0000'007F;            // IR[6:0]
+    instr.rd = (IR & 0x0000'0F80) >> 7;         // IR[11:7]
+    instr.funct3 = (IR & 0x0000'7000) >> 12;    // IR[14:12]
+    instr.rs1 = (IR & 0x000F'8000) >> 15;       // IR[19:15]
+    instr.rs2 = (IR & 0x01F0'0000) >> 20;       // IR[24:20]
+    instr.funct7 = (IR & 0xFE00'0000) >> 25;    // IR[31:25]
     instr.imm = DecodeImm(instr.opcode);
     return instr;
   }
   void CPU::Print(Instruction &instr) {
+    std::cout << "Original insturction:\n";
+    std::cout << std::bitset<32>(IR) << std::endl << std::endl;
+
     std::cout << "Instruction fields in binary:\n";
     std::cout << "------------------------------------------------\n";
-    std::cout << "opcode : " << std::bitset<7>(instr.opcode) << '\n';
-    std::cout << "rd     : " << std::bitset<5>(instr.rd) << '\n';
-    std::cout << "funct3 : " << std::bitset<3>(instr.funct3) << '\n';
-    std::cout << "rs1    : " << std::bitset<5>(instr.rs1) << '\n';
-    std::cout << "rs2    : " << std::bitset<5>(instr.rs2) << '\n';
-    std::cout << "funct7 : " << std::bitset<7>(instr.funct7) << '\n';
+    std::cout << "opcode : " << instr.opcode << '\n';
+    std::cout << "rd     : " << instr.rd << '\n';
+    std::cout << "funct3 : " << instr.funct3 << '\n';
+    std::cout << "rs1    : " << instr.rs1 << '\n';
+    std::cout << "rs2    : " << instr.rs2 << '\n';
+    std::cout << "funct7 : " << instr.funct7 << '\n';
     std::cout << "imm    : " << std::bitset<32>(instr.imm) << '\n';
     std::cout << "------------------------------------------------\n";
   }
--- CPU.h
+++ CPU.h
@@ -1,5 +1,6 @@
 #ifndef CPU_PROCESSOR
 #define CPU_PROCESSOR
+
 #include <stdint.h>
 #include <iostream>
 #include <bitset>
@@ -14,22 +15,23 @@ namespace RV32IM {
     //   public:
     //       const byte c_ReservedAddress;   const byte c_BaseAddress;   const byte c_AddressCeiling;
       private:
-          uint32_t ProgramCounter;
-          uint32_t Register[NUMBER_REGISTER];
-          uint32_t MemoryAddressRegister;
-          uint32_t MemoryDataRegister;
-          uint32_t InstructionRegister;
+        uint32_t PC;    // ProgramCounter
+        uint32_t Register[NUMBER_REGISTER];
+        uint32_t MAR;   // MemoryAddressRegister
+        uint32_t MDR;   // MemoryDataRegister
+        uint32_t IR;    // InstructionRegister
+
         //   bool m_OverflowError;
         //   bool m_UnderflowError;   
         //   bool m_SignedMode;   
         //   bool m_Halt;
 
-          Memory* TheMemory;
-          void Fetch();
-          int32_t DecodeImm(uint8_t p_Opcode);
-          Instruction Decode();
+        Memory* TheMemory;
+        void Fetch();
+        int32_t DecodeImm(std::bitset<7> p_Opcode);
+        Instruction Decode();
 
-          void Print(Instruction &instr);
+        void Print(Instruction &instr);
 
       public:
         CPU(Memory* p_TheMemory);
