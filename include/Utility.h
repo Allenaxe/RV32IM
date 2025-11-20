65e08e3de6a33e91bcf99c86eed8f889e27670a1
doctorxsx <allen31042@gmail.com>
2025-11-20 14:46:17 +0800

    Modified - utility can record output

--- include/Utility.h
+++ include/Utility.h
@@ -7,13 +7,14 @@
 
 namespace RV32IM {
 
-    using Output = std::variant<ID_EX_Data/*, ExecuteOutput, MemoryOutput, WritebackOutput*/>;
+    using Output = std::variant<IF_ID_Data, ID_EX_Data, EX_MEM_Data, MEM_WB_Data>;
 
     class Utility {
         private:
             std::vector<Output> Trace;
             
         public:
+            void Record(Output p_Output);
             void Print();
     };
 }
--- src/Utility.cpp
+++ src/Utility.cpp
@@ -2,7 +2,11 @@
 
 namespace RV32IM {
 
-     void Utility::Print() {
+    void Utility::Record(Output p_Output) {
+        Trace.push_back(p_Output);
+    }
+
+    void Utility::Print() {
         for (auto& out : Trace) {
             std::visit(Printer(), out);
         }
